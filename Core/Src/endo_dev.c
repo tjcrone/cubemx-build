75e1f83629787394b6af59377d95e5aa726ef857
Tim Crone <tjcrone@gmail.com>
2022-03-18 18:02:41 -0400

    Add format function to SD test suite

--- Core/Src/endo_dev.c
+++ Core/Src/endo_dev.c
@@ -13,7 +13,6 @@ static const char deadbeef[][16] = DEADBEEF;
 // global variables
 FRESULT fres;
 FATFS fs = { 0 };
-UINT byteswritten;
 UINT bytesread;
 BYTE work[4096];
 FIL fil;
@@ -36,53 +35,69 @@ void flash(uint32_t n, uint32_t on_time, uint32_t off_time) {
 
 // full test suite
 void sd_test(void) {
-    flash(1, 200, 200);
+    sd_format();
     HAL_Delay(1000);
+
     sd_mount();
-    flash(8, 50, 50);
     HAL_Delay(1000);
 
-    flash(2, 200, 200);
-    HAL_Delay(1000);
     sd_check_free();
-    flash(8, 50, 50);
     HAL_Delay(1000);
 
-    flash(3, 200, 200);
+    /*
+    sd_test_delete();
     HAL_Delay(1000);
+
     sd_test_write();
-    flash(8, 50, 50);
     HAL_Delay(1000);
+    */
+}
+
+// format card
+void sd_format(void) {
+    flash(1, 200, 200); // test 1
+    HAL_Delay(1000);
+    fres = f_mkfs("ENDO", FM_EXFAT, 0, work, sizeof work);
+    if (fres != FR_OK)
+        Error_Handler();
+    flash(8, 50, 50); // success
 }
 
 // mount sd card
 void sd_mount(void) {
+    flash(2, 200, 200); // test 2
+    HAL_Delay(1000);
     fres = f_mount(&fs, "", 1);
     if (fres != FR_OK)
         Error_Handler();
+    flash(8, 50, 50); // success
 }
 
 // check sd free space
 void sd_check_free(void) {
+    flash(3, 200, 200); // test 3
+    HAL_Delay(1000);
     DWORD fre_clust;
     FATFS *pfs;
     fres = f_getfree("", &fre_clust, &pfs);
     if (fres != FR_OK)
         Error_Handler();
-
     // calclulate sizes
     uint64_t volume_tot = ( (uint64_t) (pfs->n_fatent - 2) ) * pfs->csize * 512; // total volume size in bytes
     uint64_t volume_fre = ( (uint64_t) (fre_clust) ) * pfs->csize * 512; // total volume size in bytes
-
     // samsung pro 32
     if ( volume_tot != 32022691840 )
         Error_Handler();
     if ( volume_fre != 32022495232 )
         Error_Handler();
+    flash(8, 50, 50); // success
 }
 
 // write test file
 void sd_test_write(void) {
+    flash(4, 200, 200); // test 4
+    HAL_Delay(1000);
+
     // open file to write
     fres = f_open(&fil, "test.txt", FA_CREATE_ALWAYS | FA_WRITE);
     if (fres != FR_OK)
@@ -90,13 +105,29 @@ void sd_test_write(void) {
 
     // write deadbeef data to file
     uint32_t datarows = ( (uint32_t) sizeof(deadbeef) ) / 16;
+    UINT byteswritten;
     //for (uint32_t i = 0; i < datarows; i++) {
     fres = f_write(&fil, &deadbeef, 16 * datarows, &byteswritten);
     if (fres != FR_OK)
         Error_Handler();
 
     fres = f_sync(&fil);
+    if (fres != FR_OK)
+        Error_Handler();
+
     fres = f_close(&fil);
+    if (fres != FR_OK)
+        Error_Handler();
+
+    flash(8, 50, 50); // success
+}
+
+// delete test file
+void sd_test_delete(void) {
+    flash(5, 200, 200); // test 5
+    HAL_Delay(1000);
+    fres = f_unlink("test.txt");
+    flash(8, 50, 50); // success
 }
 
 /*
@@ -132,13 +163,6 @@ void sd_test_write(void) {
 
 // sd card test scratch
     /*
-    // format card
-    start = uwTick;
-    fres = f_mkfs("", FM_EXFAT, 0, work, sizeof work);
-    stop = uwTick;
-    uint32_t  format_time = stop - start;
-    if (fres != FR_OK)
-        Error_Handler();
 
 
 
